831ae3fc2f3ee9715cc43587672dc94c52de1011
James Bowen <bowen45@icloud.com>
2022-02-13 15:42:44 -0500

    added image

--- You are awsome!/ViewController.swift
+++ You are awsome!/ViewController.swift
@@ -11,22 +11,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var msgLabel: UILabel!
     
+    @IBOutlet weak var imageView1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad has run!")
         msgLabel.text = "You are"    }
 
     @IBAction func msgButtonPressed(_ sender: UIButton) {
-        print("😎The message button was pressed!")
         msgLabel.text = "You are awsome!"
         msgLabel.textColor = UIColor.red
         msgLabel.textAlignment = .left
+        imageView1.image = UIImage(named: "image0")
+        
         
     }
     @IBAction func msgButton2Pressed(_ sender: UIButton) {
         msgLabel.text = "You are great!"
         msgLabel.textColor = UIColor.green
         msgLabel.textAlignment = .right
+        imageView1.image = UIImage(named: "image1")
     }
     
 }
